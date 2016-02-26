c4ccbb8d32b02f0595c10ebc7087b974b1d68750
Yaroslav Lepenkin <yaroslav.lepenkin@jetbrains.com>
2016-02-26 17:32:57 +0300

    Logging experiment status

--- server/src/com/intellij/stats/collector/experiment/server/ExperimentServer.java
+++ server/src/com/intellij/stats/collector/experiment/server/ExperimentServer.java
@@ -4,7 +4,10 @@ import com.google.gson.Gson;
 import spark.Request;
 import spark.Response;
 
+import java.util.Date;
+import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 
 import static spark.Spark.get;
 import static spark.Spark.port;
@@ -15,6 +18,8 @@ class ExperimentInfoRoute {
     private String salt = getSalt();
     private int experimentVersion = 1;
 
+    private Map<String, DatedExperimentInfo> userExperimentInfo = new ConcurrentHashMap<>();
+
     private static String getSalt() {
         String uuid = UUID.randomUUID().toString();
         return uuid.split("-")[0];
@@ -24,9 +29,22 @@ class ExperimentInfoRoute {
         String uuid = request.params("uuid");
         int hash = (uuid + salt).hashCode();
         boolean performExperiment = hash % 2 == 0;
-        return new ExperimentInfo(experimentVersion, performExperiment);
+        ExperimentInfo info = new ExperimentInfo(experimentVersion, performExperiment);
+        userExperimentInfo.put(uuid, new DatedExperimentInfo(info));
+        return info;
     }
     
+    public String getUsers(Request request, Response response) {
+        StringBuilder builder = new StringBuilder();
+        userExperimentInfo.entrySet().forEach((e) -> {
+            String uid = e.getKey();
+            DatedExperimentInfo info = e.getValue();
+            String line = uid.split("-")[0] + " : " + info;
+            builder.append(line).append('\n');
+        });
+        return builder.toString();
+    }
+
     public Object nextExperiment(Request request, Response response) {
         salt = getSalt();
         experimentVersion++;
@@ -45,16 +63,37 @@ public class ExperimentServer {
         
         get("/experiment/info/:uuid", route::getExperimentInfo, gson::toJson);
         get("/experiment/nextExperiment", route::nextExperiment);
+        get("/experiment/users", route::getUsers);
     }
     
 }
 
+class DatedExperimentInfo {
+    public final ExperimentInfo experimentInfo;
+    public final Date date;
+
+    public DatedExperimentInfo(ExperimentInfo info) {
+        experimentInfo = info;
+        date = new Date();
+    }
+
+    @Override
+    public String toString() {
+        return experimentInfo + " " + date;
+    }
+}
+
 class ExperimentInfo {
-    private int experimentVersion = 0;
-    private boolean performExperiment = false;
+    public int experimentVersion = 0;
+    public boolean performExperiment = false;
 
     ExperimentInfo(int experimentVersion, boolean performExperiment) {
         this.experimentVersion = experimentVersion;
         this.performExperiment = performExperiment;
+    }
+
+    @Override
+    public String toString() {
+        return "Experiment=" + performExperiment + ", Version=" + experimentVersion;  
     }
 }
