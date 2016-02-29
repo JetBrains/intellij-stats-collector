670d78160a193ea5caf6749149d4a08ee63894b6
Yaroslav Lepenkin <yaroslav.lepenkin@jetbrains.com>
2016-02-29 16:43:14 +0300

    minor

--- experiment-server/src/main/java/com/intellij/stats/completion/experiment/ExperimentServer.java
+++ experiment-server/src/main/java/com/intellij/stats/completion/experiment/ExperimentServer.java
@@ -1,56 +1,11 @@
 package com.intellij.stats.completion.experiment;
 
 import com.google.gson.Gson;
-import spark.Request;
-import spark.Response;
 
 import java.util.Date;
-import java.util.Map;
-import java.util.UUID;
-import java.util.concurrent.ConcurrentHashMap;
 
 import static spark.Spark.get;
 import static spark.Spark.port;
-
-
-class ExperimentInfoRoute {
-    
-    private String salt = getSalt();
-    private int experimentVersion = 1;
-
-    private Map<String, DatedExperimentInfo> userExperimentInfo = new ConcurrentHashMap<>();
-
-    private static String getSalt() {
-        String uuid = UUID.randomUUID().toString();
-        return uuid.split("-")[0];
-    }
-
-    public ExperimentInfo getExperimentInfo(Request request, Response response) {
-        String uuid = request.params("uuid");
-        int hash = (uuid + salt).hashCode();
-        boolean performExperiment = hash % 2 == 0;
-        ExperimentInfo info = new ExperimentInfo(experimentVersion, performExperiment);
-        userExperimentInfo.put(uuid, new DatedExperimentInfo(info));
-        return info;
-    }
-    
-    public String getUsers(Request request, Response response) {
-        StringBuilder builder = new StringBuilder();
-        userExperimentInfo.entrySet().forEach((e) -> {
-            String uid = e.getKey();
-            DatedExperimentInfo info = e.getValue();
-            String line = uid.split("-")[0] + ": " + info;
-            builder.append(line).append('\n');
-        });
-        return builder.toString();
-    }
-
-    public Object nextExperiment(Request request, Response response) {
-        salt = getSalt();
-        experimentVersion++;
-        return "ok";  
-    }
-}
 
 
 public class ExperimentServer {
