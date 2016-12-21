19f68139eee6600b65186f4dc133d3f8315defb5
Yaroslav Lepenkin <yaroslav.lepenkin@jetbrains.com>
2016-12-21 17:26:52 +0300

    minor

--- completion-ranking/src/test/kotlin/com/jetbrains/completion/ranker/FeatureTransformationTest.kt
+++ completion-ranking/src/test/kotlin/com/jetbrains/completion/ranker/FeatureTransformationTest.kt
@@ -1,27 +1,20 @@
 package com.jetbrains.completion.ranker
 
-import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
 import com.jetbrains.completion.ranker.features.*
 import com.jetbrains.completion.test.DataTable
-import com.jetbrains.completion.ranker.features.readJsonMap
-
-import com.jetbrains.completion.ranker.features.CompletionData
-import com.jetbrains.completion.ranker.features.findWithSessionUid
 import com.jetbrains.completion.test.readTable
 import org.junit.Before
 import org.junit.Test
 import java.io.File
 
-val gson = Gson()
 
 fun <T> MutableList<T>.merge(another: List<T>): MutableList<T> {
     addAll(another)
     return this
 }
 
+
 class FeatureTransformationTest {
-    
     
     private lateinit var transformer: FeatureTransformer
     private lateinit var featuresOrder: Map<String, Int>
@@ -74,8 +67,7 @@ class FeatureTransformationTest {
 
         assert(cleanRaws.size == fullLookupSent.size, { "For session id: $session_id;" +
                 " Clean raws size: ${cleanRaws.size};" +
-                " Completion lookup items: ${completionItems.size}"
-        })
+                " Completion lookup items: ${completionItems.size}" })
         
         cleanRaws.zip(fullLookupSent)
                 .forEach { 
@@ -116,37 +108,7 @@ class FeatureTransformationTest {
             throw UnsupportedOperationException()
         }
     }
-    
-
-    @Test
-    fun `test xxx`() {
-        val lookup: List<Any> = readCompletionLookup("features_transformation/completion_list.json")
-        
-        val state = CompletionState(0, 1, 4, 5)
-        val array = transformer.toFeatureArray(state, lookup[0] as Map<String, Any>)
-
-        val features = readFile("features_transformation/result_feature_array.txt").split("\n")
-
-        println()
-        
-        val diff = array.toList().zip(features.map(String::toDouble)).map { it.first - it.second }
-
-        println(diff.sum())
-
-        val error = diff.indexOfFirst { Math.abs(it) > 0.1 }
-
-        println(featuresOrder.entries.find { it.value == error })
-    }
-
-
 }
-
-fun readCompletionLookup(filePath: String): List<Any> {
-    val text = readFile(filePath)
-    val typeToken = object : TypeToken<Map<String, Any>>() {}
-    return gson.fromJson<Map<String, Any>>(text, typeToken.type)["newCompletionListItems"] as List<Any>
-}
-
 
 fun getFile(fileName: String): File {
     val file = FeatureTransformationTest::class.java
@@ -155,9 +117,4 @@ fun getFile(fileName: String): File {
             .file
 
     return File(file)
-}
-
-fun readFile(fileName: String): String {
-    val file = getFile(fileName)
-    return file.readText()
 }
