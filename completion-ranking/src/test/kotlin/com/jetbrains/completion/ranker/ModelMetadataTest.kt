4426861f520a2625cdba8f48e6d29ac11d080498
Vitaliy.Bibaev <vitaliy.bibaev@jetbrains.com>
2017-12-14 14:23:01 +0300

    Add tests to check that ml model description is consistent

---
+++ completion-ranking/src/test/kotlin/com/jetbrains/completion/ranker/ModelMetadataTest.kt
@@ -0,0 +1,115 @@
+package com.jetbrains.completion.ranker
+
+import com.jetbrains.completion.ranker.features.*
+import com.jetbrains.completion.ranker.features.impl.FeatureInterpreterImpl
+import com.jetbrains.completion.ranker.features.impl.FeatureManagerFactory
+import com.jetbrains.completion.ranker.features.impl.FeatureReader
+import com.jetbrains.completion.ranker.features.impl.FeatureUtils
+import org.junit.Assert
+import org.junit.Assert.*
+import org.junit.Test
+
+/**
+ * @author Vitaliy.Bibaev
+ */
+class ModelMetadataTest {
+    @Test
+    fun `validate feature order`() {
+        val manager = featureManager()
+        val featureIndex: Map<String, Feature> = mutableMapOf<String, Feature>().apply {
+            manager.binaryFactors.forEach { put(it.name, it) }
+            manager.doubleFactors.forEach { put(it.name, it) }
+            manager.categorialFactors.forEach { put(it.name, it) }
+        }
+
+        for ((name, order) in manager.featureOrder) {
+            val split = name.split('=')
+            if (split.size > 3) Assert.fail("line '$name' in feature order contains more than 1 symbol '='")
+            if (split.size == 2) {
+                val (featureName, value) = split
+                val feature = featureIndex[featureName]
+                        ?: throw AssertionError("feature declared in the featureOrder not found: $featureName")
+                when (feature) {
+                    is BinaryFeature -> {
+                        assertEquals(FeatureUtils.UNDEFINED, value)
+                        assertEquals(order, feature.undefinedIndex)
+                    }
+                    is DoubleFeature -> {
+                        assertEquals(FeatureUtils.UNDEFINED, value)
+                        assertEquals(order, feature.undefinedIndex)
+                    }
+                    is CatergorialFeature -> {
+                        if (value == FeatureUtils.UNDEFINED) {
+                            assertEquals(order, feature.undefinedIndex)
+                        } else if (value == FeatureUtils.OTHER) {
+                            assertEquals(order, feature.otherCatergoryIndex)
+                        } else {
+                            assertEquals(order, feature.indexByCategory(value))
+                        }
+                    }
+                }
+            } else {
+
+            }
+        }
+    }
+
+    @Test
+    fun `test all features have distinct names`() {
+        val manager = featureManager()
+        val featuresByName = mutableListOf<Feature>().apply {
+            addAll(manager.binaryFactors)
+            addAll(manager.doubleFactors)
+            addAll(manager.categorialFactors)
+        }.groupBy { it.name }
+
+        var failed = false
+        for ((name, features) in featuresByName) {
+            if (features.size > 1) {
+                println("${features.size} features with name '$name found'")
+                failed = true
+            }
+        }
+
+        assertFalse("features with the same name found (see output above)", failed)
+
+    }
+
+    @Test
+    fun `test features fill features array properly`() {
+        val manager = featureManager()
+        val revertedFeatureOrder = mutableMapOf<Int, Feature>()
+        fun MutableMap<Int, Feature>.checkAndPut(index: Int, feature: Feature) {
+            assertFeaturesNotStoreValueBySameIndex(index, put(index, feature), feature)
+        }
+        for (feature in manager.binaryFactors) {
+            revertedFeatureOrder.checkAndPut(feature.index, feature)
+            revertedFeatureOrder.checkAndPut(feature.undefinedIndex, feature)
+        }
+
+        for (feature in manager.doubleFactors) {
+            revertedFeatureOrder.checkAndPut(feature.index, feature)
+            revertedFeatureOrder.checkAndPut(feature.undefinedIndex, feature)
+        }
+
+        for (feature in manager.categorialFactors) {
+            revertedFeatureOrder.checkAndPut(feature.otherCatergoryIndex, feature)
+            feature.categories.forEach {
+                revertedFeatureOrder.checkAndPut(feature.indexByCategory(it), feature)
+            }
+        }
+
+        assertEquals("features should totally cover the features array", manager.featureOrder.size, revertedFeatureOrder.size)
+        assertEquals(0, revertedFeatureOrder.keys.min())
+        assertEquals(manager.featureOrder.size - 1, revertedFeatureOrder.keys.max())
+    }
+
+    private fun assertFeaturesNotStoreValueBySameIndex(index: Int, old: Feature?, new: Feature) {
+        if (old != null) {
+            fail("features store values to the same array element [index = $index, names = {${old.name}, ${old.name}}]")
+        }
+    }
+
+    private fun featureManager(): FeatureManager =
+            FeatureManagerFactory().createFeatureManager(FeatureReader, FeatureInterpreterImpl())
+}
