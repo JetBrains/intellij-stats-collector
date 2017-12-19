b83fc3c41f0767cbfea2191844ed9133d8d6a14a
Vitaliy.Bibaev <vitaliy.bibaev@jetbrains.com>
2017-12-19 12:30:37 +0300

    Minor: remove redundant constructions

--- completion-ranking/src/main/kotlin/com/jetbrains/completion/ranker/features/ex/FeatureDefaultValueNotFound.kt
+++ completion-ranking/src/main/kotlin/com/jetbrains/completion/ranker/features/ex/FeatureDefaultValueNotFound.kt
@@ -17,5 +17,4 @@
 package com.jetbrains.completion.ranker.features.ex
 
 class FeatureDefaultValueNotFound(name: String)
-    : IllegalArgumentException("Feature default value not found. Feature name: $name") {
-}
+    : IllegalArgumentException("Feature default value not found. Feature name: $name")
