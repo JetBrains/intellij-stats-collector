8870211fdf16264bcf6d848b21bb1a9fa5cc3cbd
Vitaliy.Bibaev <vitaliy.bibaev@jetbrains.com>
2018-05-04 17:51:26 +0300

    EA-119755 Fix storage corrupted exception (prevent possible corruption)

--- src/com/intellij/stats/ngram/NGramEnumeratingService.kt
+++ src/com/intellij/stats/ngram/NGramEnumeratingService.kt
@@ -3,6 +3,7 @@ package com.intellij.stats.ngram
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.application.PathManager
 import com.intellij.openapi.components.ServiceManager
+import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.util.io.FileUtilRt
 import com.intellij.util.containers.ContainerUtil
 import com.intellij.util.io.PagedFileStorage
@@ -10,8 +11,9 @@ import com.intellij.util.io.PersistentEnumeratorBase
 import com.intellij.util.io.PersistentStringEnumerator
 import java.io.File
 
-class NGramEnumeratingService : Disposable {
 
+class NGramEnumeratingService : Disposable {
+    private val LOG = logger<NGramFileBasedIndex>()
     private val stringFile = File(File(PathManager.getIndexRoot(), KEY), "strings")
 
     private val stringEnumerator: PersistentStringEnumerator
@@ -24,8 +26,10 @@ class NGramEnumeratingService : Disposable {
         stringEnumerator = try {
             PersistentStringEnumerator(stringFile, storageLockContext)
         } catch (e: PersistentEnumeratorBase.CorruptedException) {
-            FileUtilRt.delete(stringFile)
+            LOG.warn("String enumeration file was corrupted. Creating a new file and restarting ngram indexing...")
+            FileUtilRt.delete(stringFile.parentFile)
             FileUtilRt.createIfNotExists(stringFile)
+            NGramFileBasedIndex.requestRebuild()
             PersistentStringEnumerator(stringFile, storageLockContext)
         }
 
@@ -35,26 +39,23 @@ class NGramEnumeratingService : Disposable {
         return stringCache.computeIfAbsent(s, { enumerateStringImpl(it) })
     }
 
-    fun valueOf(index: Int): String {
-        try {
-            storageLockContext.lock()
-            return stringEnumerator.valueOf(index) ?: throw IllegalArgumentException(index.toString())
-        } finally {
-            storageLockContext.unlock()
-        }
+    fun valueOf(index: Int): String = runWithLock {
+        stringEnumerator.valueOf(index) ?: throw IllegalArgumentException(index.toString())
     }
 
-    private fun enumerateStringImpl(s: String): Int {
-        try {
-            storageLockContext.lock()
-            return stringEnumerator.enumerate(s)
-        } finally {
-            storageLockContext.unlock()
-        }
-    }
+    private fun enumerateStringImpl(s: String): Int = runWithLock { stringEnumerator.enumerate(s) }
 
     override fun dispose() {
-        stringEnumerator.close()
+        runWithLock { stringEnumerator.close() }
+    }
+
+    private fun <T> runWithLock(action: () -> T): T {
+        try {
+            storageLockContext.lock()
+            return action()
+        } finally {
+            storageLockContext.unlock()
+        }
     }
 
     companion object {
